32059528fc5e1b3d3b66a8d83ceb61ab16639247
kenta yamazaki <kenta_yamazaki@rosso-tokyo.co.jp>
2022-01-12 10:53:27 +0900

    Initial Commit

---
+++ StartSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StartSwiftUI
+//
+//  Created by 山崎健太 on 2022/01/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
